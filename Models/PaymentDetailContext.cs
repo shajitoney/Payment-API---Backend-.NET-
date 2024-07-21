53f7fc1aa21797cc166cc1af595152eab7dc8329
shajitoney <164307060+shajitoney@users.noreply.github.com>
2024-07-21 18:38:15 +0530

    Add files via upload

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext 
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options) //constructor 
+
+        {
+
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
